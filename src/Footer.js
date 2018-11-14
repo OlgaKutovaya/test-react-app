6f73e5308c0dfb557a3ca295be9b311181b0c91b
OlgaKutovaya <o.kutovaya.ak@gmail.com>
2018-11-14 12:53:25 +0300

    components were changed

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,42 @@
 import React, {Component} from 'react';
 
 class Footer extends Component {
+    state = {
+        isChecked: false
+    };
+
     render() {
         return (
             <footer>
                 <label className="hide-items-wrapper">
-                    <input id="checkboxHideItem" type="checkbox"/>
+                    <input
+                        id="checkboxHideItem"
+                        type="checkbox"
+                        checked={this.state.isChecked}
+                        onClick={event => {
+                            this.setState(prevstate => {
+                                return {
+                                    isChecked: !prevstate.isChecked
+                                }
+                            }, () => {
+                                this.props.hideDoneItems(this.state.isChecked);
+                            });
+                        }}
+                    />
                     <span>Hide all completed tasks</span>
                 </label>
-                <button id="btnDelAllCompleted">delete all completed tasks</button>
-                <button id="btnDelAll">delete all tasks</button>
+                <button
+                    id="btnDelAllCompleted"
+                    onClick={event => {
+                        this.props.deleteAllCompletedItems();
+                    }}
+                >delete all completed tasks</button>
+                <button
+                    id="btnDelAll"
+                    onClick={event => {
+                        this.props.delAllItems();
+                    }}
+                >delete all tasks</button>
             </footer>
         );
     }
